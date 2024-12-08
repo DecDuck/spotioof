75d083fb7f19442197c5abad3175a358f5d0048d
DecDuck <declanahofmeyr@gmail.com>
2024-12-08 14:54:02 +1100

    feat(tracks): import from library folder

---
+++ import.ts
@@ -0,0 +1,28 @@
+import { PrismaClient, Record } from "@prisma/client";
+import * as musicMetadata from "music-metadata";
+
+import fs from "fs";
+import path from "path";
+
+const prisma = new PrismaClient();
+const libraryDir = path.join(".", "library");
+
+const existingLibrary = await prisma.record.findMany({});
+const localLibrary = fs.readdirSync(libraryDir);
+
+for (const localFilename of localLibrary) {
+  const index = existingLibrary.findIndex((e) => e.filename == localFilename);
+  if (index != -1) continue; // If the record already exists, skip
+
+  const fullFilepath = path.join(libraryDir, localFilename);
+  const metadata = await musicMetadata.parseFile(fullFilepath);
+
+  const trackName = metadata.common.title ?? localFilename;
+
+  await prisma.record.create({
+    data: {
+      filename: localFilename,
+      trackName: trackName,
+    },
+  });
+}
